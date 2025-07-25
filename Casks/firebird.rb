70ee825db47ca3d11131ed48743246232a7b8fa7
tmmamede <thiago.mamede@outlook.com.br>
2025-07-25 11:02:42 -0300

    Update firebird.rb

--- Casks/firebird.rb
+++ Casks/firebird.rb
@@ -1,8 +1,8 @@
 cask "firebird" do
-  version "5.0.3.1683"
+  version "5.0.3"
   sha256 "07e06ba4c0f13c9c39b65b854d1e66b59bfcfbd1513c22e753f2d32421cc7fad"
 
-  url "https://github.com/FirebirdSQL/firebird/releases/download/v#{version}/Firebird-#{version}-0-macos-arm64.pkg",
+  url "https://github.com/FirebirdSQL/firebird/releases/download/v#{version}/Firebird-#{version}.1683-0-macos-arm64.pkg",
       verified: "github.com/FirebirdSQL/firebird/"
   name "Firebird"
   desc "Relational SQL database management system"
